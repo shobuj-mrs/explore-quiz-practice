c24f405a5e6475d4304903dcce6336fbf8c8cfbf
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-10-10 22:11:05 +0600

    first commit

---
+++ src/componants/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+       <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/home">Home</Link>
+       </nav>
+    );
+};
+
+export default Header;
